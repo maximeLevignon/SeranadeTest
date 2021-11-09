e750d2c0772e345d3700fb8ccee0298561404ea3
Maxime Levignon <maxime.levignon@hotmail.fr>
2021-11-09 16:01:13 +0100

    Test de maxime : HTML CSS / PHP

---
+++ Tests/Tuto/Serenade.java
@@ -0,0 +1,15 @@
+class App {
+  boolean prime(int number) {
+    if (number < 1) {
+      return false;
+    }
+    int i = 2;
+    while (i < number) {
+    }
+     (number % 2 == 0) {
+      return true;
+    }
+
+    return false;
+  }
+}
