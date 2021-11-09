6fd83c3d085f99c54f672ca259b8321e78e5debc
Louis TA <louista91@hotmail.fr>
2021-11-09 16:58:27 +0100

    Test serenade javascript HW/Complex

---
+++ Tests/Javascript/Complex.js
@@ -0,0 +1,70 @@
+/*
+<!DOCTYPE html>
+<title>My Example</title>
+
+<script>
+document.addEventListener("DOMContentLoaded", functions(event)) {
+
+  // var e = Documents.getElementsById("jumpmenu");
+
+  e.addEventListener("change", function() {
+       selectedUrl = this.options[this.options.selectedIndex].value;
+
+       if(this.value != "null") {
+
+       var confirmLeave = confirm("Are you sure?");
+
+         if (confirmLeave == true) {
+
+           document.location.href = selectedUrl;
+
+
+  )
+</script>
+</!DOCTYPE html>
+*/
+
+/* Commandes
+ insert Tag exclamation point caps DOCTYPE space html
+ insert tag title
+ insert capital my space capital example
+ insert tag script
+ type document
+    insert dot addEventListener parens quotes dom content loaded
+    insert comma functions parens event
+    insert brace
+ insert var e equals
+    insert document dot getelementbyid parens quotes
+    type jumpmenu
+    insert semicolon
+ insert e
+    insert dot addEventListener parens quotes
+    type change
+    insert comma function parens
+    insert brace
+ insert selectedUrl equals
+    insert this dot options bracket
+    insert this dot options dot selectedIndex
+    insert dot value semicolon
+ insert if parens
+    insert this dot value exclamation point equals
+    insert quotes null
+    insert brace
+ insert var confirmLeave equals
+    insert confirm parens quotes capital are you sure question mark
+    insert semicolon
+ insert if parens
+    insert confirmLeave equals equals true
+    insert brace
+
+
+ */
+
+/* Commentaires
+    - si on change de ligne en ayant laissé des espaces, compliqué de revenir au bout de la ligne
+ */
+
+/*
+ratios caractères caractèresCode/caractèreDit
+ */
+
---
+++ Tests/Javascript/HelloWorld.js
@@ -0,0 +1,8 @@
+//document.write('Hello, world!');
+//insert document dot write parens apostrophy hello comma world exclamation point apostrophy semicolon
+document.write('hello, world!');
+/*
+Erreur de traduction sur semicolon
+ */
+
+
