99a21958fba2577e7e6c6fc481722c522934991e
Louis TA <louista91@hotmail.fr>
2021-11-16 11:03:30 +0100

    Demo présentation

---
+++ Tests/C/Demo.c
@@ -0,0 +1,16 @@
+for (int i = 0; i < 10; ++i) {
+    printf("%s\t%d\n", (ptr + i)->a, (ptr + i)->b);
+  }
+
+add for int i equals zero semicolon i less than ten semicolon plus plus i
+
+add one word printf of quotes percent s backslash t percent d backslash n
+    insert coma of p t r plus i arrow a
+    insert coma of p t r plus i arrow b
+
+main(void) {
+}
+
+for (int i = 0; i < 10; ++i) {
+  printfof "%s\t%d\n";, (ptr + i)->a
+}
